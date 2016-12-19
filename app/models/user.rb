6603b472ad0dee53e1a3aa56cbf30249504bbd0e
Joe Gaebel <joe.gaebel@hotmail.com>
2016-12-18 22:57:37 -0500

    Add validations for presence of names and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   validates_uniqueness_of :mistaken_name, case_sensitive: false
-  validates_presence_of :avatar
+  validates_presence_of :mistaken_name, :preferred_name, :avatar
 end
