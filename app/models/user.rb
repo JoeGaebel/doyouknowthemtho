1289c8c14176c4fded4ce9ee591ee988c6e8b965
Joe Gaebel <joe.gaebel@hotmail.com>
2016-12-18 22:16:11 -0500

    enforce avatar uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
-  validates_uniqueness_of :mistaken_name
+  validates_uniqueness_of :mistaken_name, case_sensitive: false
+  validates_presence_of :avatar
 end
