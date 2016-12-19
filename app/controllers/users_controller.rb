e6476fffadd083833e7f904d8701b94a860a46b6
Joe Gaebel <joe.gaebel@hotmail.com>
2016-12-18 22:56:45 -0500

    remove json responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,10 +42,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,10 +52,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -66,7 +62,6 @@ class UsersController < ApplicationController
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -78,6 +73,9 @@ class UsersController < ApplicationController
 
 
     def user_params
+      not_allowed = /[^A-Za-z\s]/
+      params["user"]["preferred_name"].gsub!(not_allowed, '')
+      params["user"]["mistaken_name"].gsub!(not_allowed, '')
       params.require(:user).permit(:preferred_name, :mistaken_name, :avatar)
     end
 end
