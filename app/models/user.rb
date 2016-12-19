0afbb7848555ad8faa707590acb5d20e2e8021b9
Joe Gaebel <joe.gaebel@hotmail.com>
2016-12-18 23:06:30 -0500

    Add differentiation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   validates_uniqueness_of :mistaken_name, case_sensitive: false
   validates_presence_of :mistaken_name, :preferred_name, :avatar
+  validate :names_not_same
+
+  def names_not_same
+    errors.add(:mistaken_name, "can't be the same as Perferred Name") if mistaken_name == preferred_name
+  end
 end
